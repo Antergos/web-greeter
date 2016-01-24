1d5fb2f2275a141794e33156fd48cc23d8141428
Dustin Falgout <dustin@falgout.us>
2016-01-24 11:21:03 -0600

    enable accelerated compositing and webgl

--- src/lightdm-webkit2-greeter.c
+++ src/lightdm-webkit2-greeter.c
@@ -105,9 +105,9 @@ create_new_webkit_settings_object(void) {
 		"enable-dns-prefetching", TRUE,
 		"javascript-can-open-windows-automatically", TRUE,
 		"allow-file-access-from-file-urls", TRUE,
-		"enable-accelerated-2d-canvas", FALSE,
+		"enable-accelerated-2d-canvas", TRUE,
 		"enable-smooth-scrolling", FALSE,
-		"enable-webgl", FALSE,
+		"enable-webgl", TRUE,
 		"enable-write-console-messages-to-stdout", TRUE,
 		NULL
 	);
