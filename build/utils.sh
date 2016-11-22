059f2db64d417fd658990f27e7d2053902496dde
Dustin Falgout <dustin@falgout.us>
2016-11-22 01:54:04 -0600

    dont build docker image if we already have a cached copy of it.

---
+++ build/utils.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+combine_javascript_sources() {
+	cd "${MESON_SOURCE_ROOT}/src/gresource/js" && {
+		cat _vendor/moment-with-locales.min.js \
+			LightDMObjects.js \
+			Greeter.js \
+			GreeterConfig.js \
+			ThemeUtils.js \
+			ThemeHeartbeat.js > bundle.js
+	}
+}
+
+list_javascript_sources() {
+	cd "${MESON_SOURCE_ROOT}/src" && find gresource/js -type f -name '*.js' -print
+}
+
+
+case "$1" in
+	combine-js)
+		combine_javascript_sources
+	;;
+
+	get-js-files)
+		list_javascript_sources
+	;;
+esac
