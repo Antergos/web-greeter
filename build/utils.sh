dc96e64096094023022b5be59a59c072875909e3
Dustin Falgout <dustin@falgout.us>
2016-12-01 06:59:57 -0600

    update utils script

--- build/utils.sh
+++ build/utils.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+do_build() {
+	(cd "$(dirname "${DIR}")" \
+		&& meson build \
+		&& cd build \
+		&& ninja)
+}
+
+clean_build_dir() {
+	(cd "${DIR}" \
+		&& find . -type f ! -path './ci*' ! -name '.gitignore' ! -name utils.sh -delete \
+		&& find . -type d ! -path './ci' -delete \
+		&& { rm ../src/gresource/js/bundle.js || true; })
+}
 
 combine_javascript_sources() {
 	cd "${MESON_SOURCE_ROOT}/src/gresource/js" && {
@@ -24,5 +40,13 @@ case "$1" in
 
 	get-js-files)
 		list_javascript_sources
+	;;
+
+	clean-build-dir)
+		clean_build_dir
+	;;
+
+	build)
+		clean_build_dir && do_build
 	;;
 esac
