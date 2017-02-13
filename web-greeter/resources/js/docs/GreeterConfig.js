cb0defc64f56135bdefc71994b73d0d5e29f4a43
Dustin Falgout <dustin@falgout.us>
2017-02-12 22:13:18 -0600

    Fix name in copyright headers

--- web-greeter/resources/js/docs/GreeterConfig.js
+++ web-greeter/resources/js/docs/GreeterConfig.js
@@ -3,14 +3,14 @@
  *
  * Copyright © 2017 Antergos Developers <dev@antergos.com>
  *
- * This file is part of web-greeter.
+ * This file is part of Web Greeter.
  *
- * web-greeter is free software; you can redistribute it and/or modify
+ * Web Greeter is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * web-greeter is distributed in the hope that it will be useful,
+ * Web Greeter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
--- web-greeter/resources/js/docs/LightDMObjects.js
+++ web-greeter/resources/js/docs/LightDMObjects.js
@@ -3,14 +3,14 @@
  *
  * LightDMObjects.js
  *
- * This file is part of web-greeter
+ * This file is part of Web Greeter
  *
- * web-greeter is free software: you can redistribute it and/or modify
+ * Web Greeter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License,
  * or any later version.
  *
- * web-greeter is distributed in the hope that it will be useful,
+ * Web Greeter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
